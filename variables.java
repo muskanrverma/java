f7157cc527c61a6dbc72b5b7a8bf7990025f92f9
muskanrverma <imuskanverma19@gmail.com>
2026-01-07 22:41:04 +0530

    program 2

---
+++ variables.java
@@ -0,0 +1,34 @@
+class variables {
+    public static void main(String[] args) {
+       // variable declaration statements
+        String name;
+        short salary;
+        long phno;
+        char grade;
+        byte age;
+        float cgpa;
+        boolean isAlive;
+        String degree;
+
+        //variable initialization statements
+        name = "Muskan Verma";
+        salary = 5000;
+        phno = 9874563210l;
+        grade = 'A';
+        age = 21;
+        cgpa = 9.57f;
+        isAlive = true;
+        degree = "BE";
+
+        //printing 
+        System.out.println(name);
+        System.out.println(salary);
+        System.out.println(phno);
+        System.out.println(grade);
+        System.out.println(age);
+        System.out.println(cgpa);
+        System.out.println(isAlive);
+        System.out.println(degree);
+    }
+    
+}
