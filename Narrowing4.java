da423760d24e8984c7499c01edc9ccb23fb83f7b
muskanrverma <imuskanverma19@gmail.com>
2026-01-13 10:16:45 +0530

    program 17

---
+++ Narrowing4.java
@@ -0,0 +1,21 @@
+//narrowing for int 
+
+class Narrowing4 {
+    public static void main(String[] args){
+        {
+            char c = (char) 45;
+            System.out.println("int to char " + c);
+
+        }
+        {
+            short s = (short)728;
+            System.out.println("int to short " + s);
+        }
+        {
+            byte b = (byte)122;
+            System.out.println("int to byte " + b);
+        }
+        
+    }
+    
+}
