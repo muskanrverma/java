334dc1888d8687bf1d7a8e55b7f735f3210abab8
muskanrverma <imuskanverma19@gmail.com>
2026-01-13 10:02:08 +0530

    program 16

---
+++ Narrowing3.java
@@ -0,0 +1,19 @@
+// narrowing for long 
+class Narrowing3 {
+    public static void main(String[] args)
+    {
+        {
+            int i = (int)8564l;
+            System.out.println("long to int " +i);
+        }
+        {
+            short s = (short)5426l;
+            System.out.println("long to short " + s);
+        }
+        {
+            byte b = (byte)54l;
+            System.out.println("long to byte " + b);
+        }
+    }
+    
+}
