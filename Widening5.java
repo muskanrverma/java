830c457c9405c1004064d4307688c976d4e4d144
muskanrverma <imuskanverma19@gmail.com>
2026-01-13 09:10:47 +0530

    program 13

---
+++ Widening5.java
@@ -0,0 +1,15 @@
+// widening is type of primitive typecasting 
+//for float
+
+class Widening5 {
+    public static void main(String[] args)
+    {
+        {
+            float f = 552.65f;
+            double d = f;
+            System.out.println("float " + f);
+            System.out.println("to double "+ d); //  output 552.6500244140625 
+        }
+    }
+    
+}
